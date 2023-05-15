625bc2e0ab99bbf63de1be787cafe87829f19c85
JuCarRoMar <ryokuma2983@gmail.com>
2023-05-15 15:19:38 +0000

    2023-05-15 Ejemplo-07

---
+++ Ejemplo-07-FuncionesPropias.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- Ejemplo-01-FuncionesPropias -->
+    <?php
+    // ZONA DE FUNCIONES
+    /* Tipos de funciones
+    a) Sin entrada y sin salida
+    b) Con entrada y sin salida
+    c) Sin entrada y con salida
+    d) Con entrada y con salida
+    */
+
+    # Funcion suma Sin Entrada y Sin salida
+    function suma() {
+        $num1 = 10;
+        $num2 = 20;
+        $rdo = $num1 + $num2;
+        echo "La suma es: $rdo <br>";
+        //echo "La suma es : " . ($num1 + $num2);
+    }
+
+    # Funcion resta Con entrada y Sin salida
+    function resta($num1, $num2) {
+        $rdo = $num1 - $num2;
+        echo "La resta es: $rdo <br>";
+    }
+
+    # Funcion producto Sin entrada con salida
+    function producto() {
+        $num1 = 4;
+        $num2 = 6;
+        $rdo = $num1 * $num2;
+        return "El producto es: $rdo <br>";
+        }
+
+    # Funcion division Con entrada y Con salida
+    function division($num1, $num2) {
+        $rdo = $num1 / $num2;
+        return "La division es: $rdo <br>";
+        }
+    ?>
+
+    <?php
+    // SCRIPT PRINCIPAL
+    suma();
+
+    // Tiene en cuenta la posicion
+    $n1 = 24;
+    $n2 = 16;
+    resta($n1, $n2);
+
+    // NO tiene en cuenta la posicion del parametro
+    $num1 = 12;
+    $num2 = 8;
+    resta($num2, $num1);
+
+    // Funcion producto
+    echo producto();
+
+    // Funcion division
+    echo division($n1, $n2);
+    
+    ?>
+    
+</body>
+</html>
