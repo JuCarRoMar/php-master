0e4570dc0f76912d44cb86ee1cf23c4f5896cd83
Juank <nikoglee@gmail.com>
2023-06-05 17:45:13 +0000

    2023-06-05 plantilla PHP BBDD

---
+++ a-Plantilla-CRUD.php
@@ -0,0 +1,111 @@
+<?php
+// Gestionar errores
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+?>
+
+<?php
+// Crear funciones
+function conectar($host, $usuario, $clave, $bbdd)
+{
+    // Creamos la conexión
+    $conexion = mysqli_connect($host, $usuario, $clave, $bbdd);
+    // Si conexión-> TRUE, todo correcto!
+    // Si conexión-> FALSE, error!
+    if (!$conexion) {
+        // Mostrar mensaje de error
+        echo "Error mysqli_connect_errorno(): mysqli_connect_error() . <br>";
+    }
+    return $conexion;
+}
+
+function desonectar($conexion)
+{
+    // Cerramos la conexión
+    mysqli_close($conexion);
+}
+
+// Probamos la conexión
+$conexion = conectar("localhost", "root", "root", "discoLuis");
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="bootstrap.min.css" rel="stylesheet">
+    <script src="bootstrap.bundle.min.js"></script>
+    <style>
+        h1,
+        h2,
+        p {
+            margin-top: 10px;
+            margin-left: 10px;
+        }
+
+        h1 {
+            color: white;
+        }
+    </style>
+
+</head>
+<?php
+// Crear funciones
+?>
+
+<body class="bg-dark">
+    <?php
+    /* Lógica de la página */
+    // Llamar funciones
+    if (isset($_REQUEST['enviar'])) {
+        $nombre = $_POST['nombre'];    // Linea 37 y linea 49, no tienen porque llamarse igual que el label
+    }
+    ?>
+
+    <!-- plantilla.php Con Bootstrap 5.3 -->
+    <h1>Plantilla</h1>
+    <main class="container">
+        <section class="row">
+            <h2 class="col-4 bg-warning rounded-pill text-white">Alerta</h2>
+            <p class="col-9 alert alert-warning">
+                <?php
+                // Mostrar funciones
+                if (isset($_REQUEST['enviar'])) {
+                    echo $nombre;
+                }
+                ?>
+            </p>
+        </section>
+
+        <section class="row">
+            <h2 class="col-6 bg-info rounded-pill text-white">Formulario</h2>
+            <hr>
+            <form class="col-9 bg-light p-3 rounded" method="post" action="#">
+                <label for="nombre" class="form-label">Nombre</label>
+                <input type="text" class="form-control" id="nombre" name="nombre" class="form-control">
+                <hr>
+                <input type="submit" value="enviar" name="enviar" class="btn btn-primary">
+            </form>
+        </section>
+        <hr>
+
+        <nav>
+            <p><a href="#" class="btn btn-success">Ir a página</a></p>
+
+        </nav>
+
+    </main>
+
+</body>
+
+</html>
+
+<?php
+// Al terminar la página desconectamos
+desonectar($conexion);
+?>
